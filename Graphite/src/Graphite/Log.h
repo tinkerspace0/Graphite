0152563850a00132b4bced4e727fea1452c17028
George Gil <136652865+georgegil3902@users.noreply.github.com>
2024-10-23 20:18:58 +0200

    modified Log Macros

--- Graphite/src/Graphite/Log.h
+++ Graphite/src/Graphite/Log.h
@@ -23,17 +23,17 @@ namespace Graphite
 
 
 // Core Log Macros
-#define GF_CORE_ERROR(...)		::Graphite::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define GF_CORE_WARN(...)		::Graphite::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define GF_CORE_INFO(...)		::Graphite::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define GF_CORE_TRACE(...)		::Graphite::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define GF_CORE_FATAL(...)		::Graphite::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define GF_CORE_TRACE(...)	::Graphite::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define GF_CORE_INFO(...)	::Graphite::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define GF_CORE_WARN(...)	::Graphite::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define GF_CORE_ERROR(...)	::Graphite::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define GF_CORE_FATAL(...)	::Graphite::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
 // Client Log Macros
-#define GF_CLIENT_ERROR(...)	::Graphite::Log::GetClientLogger()->error(__VA_ARGS__)
-#define GF_CLIENT_WARN(...)		::Graphite::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define GF_CLIENT_INFO(...)		::Graphite::Log::GetClientLogger()->info(__VA_ARGS__)
-#define GF_CLIENT_TRACE(...)	::Graphite::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define GF_CLIENT_FATAL(...)	::Graphite::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define GF_TRACE(...)		::Graphite::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define GF_INFO(...)		::Graphite::Log::GetClientLogger()->info(__VA_ARGS__)
+#define GF_WARN(...)		::Graphite::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define GF_ERROR(...)		::Graphite::Log::GetClientLogger()->error(__VA_ARGS__)
+#define GF_FATAL(...)		::Graphite::Log::GetClientLogger()->fatal(__VA_ARGS__)
 
 
