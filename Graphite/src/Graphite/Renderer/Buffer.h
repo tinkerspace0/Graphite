a3b45083b90e264e13d9ec779e148bf71773c003
George Gil <136652865+georgegil3902@users.noreply.github.com>
2024-11-18 12:27:18 +0100

    added missing setData function to buffer interface

--- Graphite/src/Graphite/Renderer/Buffer.h
+++ Graphite/src/Graphite/Renderer/Buffer.h
@@ -126,6 +126,8 @@ namespace Graphite {
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
+		virtual void SetData(const void* data, uint32_t size) = 0;
+
 		virtual uint32_t GetCount() const = 0;
 
 		static Ref<IndexBuffer> Create(uint32_t size);
