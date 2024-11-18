98b025e826084b5d5232a18780d10f685c8bb6c3
George Gil <136652865+georgegil3902@users.noreply.github.com>
2024-11-18 10:53:05 +0100

    updated buffer interface to create dynamic index buffer using opengl

--- Graphite/src/Graphite/Renderer/Buffer.cpp
+++ Graphite/src/Graphite/Renderer/Buffer.cpp
@@ -7,6 +7,18 @@
 
 namespace Graphite {
 
+	Ref<IndexBuffer> IndexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    GF_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLIndexBuffer>(size);
+		}
+
+		GF_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
 	{
 		switch (Renderer::GetAPI())
--- Graphite/src/Graphite/Renderer/Buffer.h
+++ Graphite/src/Graphite/Renderer/Buffer.h
@@ -128,6 +128,7 @@ namespace Graphite {
 
 		virtual uint32_t GetCount() const = 0;
 
+		static Ref<IndexBuffer> Create(uint32_t size);
 		static Ref<IndexBuffer> Create(uint32_t* indices, uint32_t count);
 	};
 
