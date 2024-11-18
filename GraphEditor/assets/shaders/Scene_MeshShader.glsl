087bb2b03883fef44e9e6039835c890563e1e3ae
George Gil <136652865+georgegil3902@users.noreply.github.com>
2024-11-18 13:04:20 +0100

    fixed Mesh shader vertex location reference

--- GraphEditor/assets/shaders/Scene_MeshShader.glsl
+++ GraphEditor/assets/shaders/Scene_MeshShader.glsl
@@ -7,8 +7,8 @@
 #version 450 core
 
 layout(location = 0) in vec3 a_Position;
-layout(location = 2) in vec3 a_Normal;
-layout(location = 1) in vec4 a_Color;
+layout(location = 1) in vec3 a_Normal;
+layout(location = 2) in vec4 a_Color;
 
 layout(std140, binding = 0) uniform Camera
 {
