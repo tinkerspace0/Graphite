7b0c36a501d1eb9ed3e4935b072c19110687e9c3
George Gil <136652865+georgegil3902@users.noreply.github.com>
2024-11-18 10:48:52 +0100

    added Mesh class

---
+++ Graphite/src/Graphite/Geometry/Mesh.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+
+namespace Graphite
+{
+
+    class Mesh {
+    public:
+
+        struct Vertex {
+            glm::vec3 Position;
+            glm::vec3 Normal;
+            glm::vec4 Color;
+        };
+
+        // Constructors
+        Mesh() = default;
+        Mesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices)
+            : m_Vertices(vertices), m_Indices(indices) {}
+        ~Mesh() {}
+
+        // Accessors
+        inline const std::vector<Vertex>& GetVertices() const { return m_Vertices; }
+        inline const std::vector<uint32_t>& GetIndices() const { return m_Indices; }
+
+        // Utility methods
+        inline size_t GetVertexCount() const { return m_Vertices.size(); }
+        inline size_t GetIndexCount() const { return m_Indices.size(); }
+
+    private:
+        std::vector<Vertex> m_Vertices;
+        std::vector<uint32_t> m_Indices;
+    };
+}
